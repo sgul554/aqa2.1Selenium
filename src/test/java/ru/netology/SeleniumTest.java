4e25aba82cc22cc22b7036d2d3743c0fe3a5fd4f
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-11-26 22:56:35 +0300

    first commit

---
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -0,0 +1,57 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSendRequestToTheCardTest() {
+        driver.get("http://localhost:9999");
+        WebElement findElement = driver.findElement(By.cssSelector("[data-test-id]"));
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ярослав Гуляев");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79265432111");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] input")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success] input")).getText();
+        assertEquals("Ваша заявка успешно отправлена!",text.trim());
+
+    }
+
+}
+
+
+
