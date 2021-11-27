13bd94bbb4b9cc49b97fe53728cfe66a1377145c
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-11-27 11:34:05 +0300

    test3

--- src/test/java/ru/netology/SeleniumTest.java
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -25,11 +25,12 @@ public class SeleniumTest {
 
     @BeforeEach
     void setupTest() {
+        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+
     }
 
     @AfterEach
